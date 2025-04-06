714073b7798b1a4925b8b70d808969da99beb1a0
vaporiiise <142332680+vaporiiise@users.noreply.github.com>
2025-04-06 19:16:07 +0800

    u81
    added deathstare

--- Assets/Scripts/Derrick's/EndSCControl.cs
+++ Assets/Scripts/Derrick's/EndSCControl.cs
@@ -29,7 +29,7 @@ public class EndSCControl : MonoBehaviour
         AudioListener.pause = false;
 
         // Restart the current scene
-        SceneManager.LoadScene(1); 
+        SceneManager.LoadScene(2); 
     }
 
     public void RestartBossFight()
